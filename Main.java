5127b4c3aa55919e816cdf6b8488a98d1b7db6cf
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-18 17:58:08 -0600

    Agregue el switch para el menu de opciones

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Seccion sec1 = new Seccion("Productos Electrónicos", 200000.0);
         Seccion sec2 = new Seccion("Ropa", 150000.0);
         Seccion sec3 = new Seccion("Alimentos", 250000.0);
@@ -10,5 +13,32 @@ public class Main {
                            "2. Mostrar Información de los Contenedores \n" +
                            "3. Mover Contenedores \n" +
                            "4. Calcular el Peso Total por Sección \n");
+        System.out.println("Ingrese una opción");
+        int op = scanner.nextInt();
+
+        switch (op) {
+            case 1:
+                System.err.println("Almacenar Contenedores");
+                
+                break;
+            case 2:
+                System.err.println("Mostrar Información de los Contenedores");
+                break;
+
+            case 3:
+                System.err.println("Mover Contenedores");
+                break;
+
+            case 4:
+                System.err.println("Calcular el Peso Total por Sección");
+                break;
+        
+            default:
+                System.err.println("Elija una opción válida");
+                break;
+        }
+
+        scanner.close();
+
     }
 }
