2697721a28c46ed565cc4350ae197b32d0524d08
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-18 17:15:54 -0600

    Agregue el Main y los objetos de seccion

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Seccion sec1 = new Seccion("Productos Electrónicos", 200000);
+        Seccion sec2 = new Seccion("Ropa", 150000);
+        Seccion sec3 = new Seccion("Alimentos", 250000);
+        Seccion sec4 = new Seccion("Maquinaria", 300000);
+
+        
+    }
+}
