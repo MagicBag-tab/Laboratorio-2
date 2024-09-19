adee9efad1354f3b7ab1e40a84620b46e0149b7d
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-18 18:21:42 -0600

    Ya esta el metodo de agregar contenedor

--- Seccion.java
+++ Seccion.java
@@ -2,6 +2,7 @@ public class Seccion {
     String nombre;
     Double capacidad;
     Contenedor[][] contenedores;
+    double pesot = 0;
 
     public Seccion(String nombre, Double capacidad){
         this.nombre = nombre;
@@ -9,9 +10,22 @@ public class Seccion {
         this.contenedores = new Contenedor[10][5];
     }
 
-    public void agregarContenedor(){
-
+    public boolean agregarContenedor(Contenedor contenedor){
+        if (pesot + contenedor.getPeso() > capacidad){
+            return false;
+        }
+        for(int fila = 0; fila < contenedores.length; fila++){
+            for(int columna = 0; columna <contenedores[fila].length; columna++){
+                if (contenedores[fila][columna] == null){
+                    contenedores[fila][columna] = contenedor;
+                    pesot = pesot + contenedor.getPeso();
+                    return true;
+                }
+            }
+        }
+        return false;
     }
+    
     public void moverContenedor(){
 
     }
