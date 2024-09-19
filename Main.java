8d60a591e8905713bb46b832b13585827e4454b4
MagicBag-tab <est24347@uvg.edu.gt>
2024-09-18 18:40:45 -0600

    Case 1 de agregar contenedores casi terminado

--- Main.java
+++ Main.java
@@ -18,23 +18,68 @@ public class Main {
 
         switch (op) {
             case 1:
-                System.err.println("Almacenar Contenedores");
-                
+                System.out.println("Almacenar Contenedores");
+                System.out.println("Seleccione una sección: \n" +
+                "1. Productos Electrónicos \n" +
+                "2. Ropa \n" +
+                "3. Alimentos \n" +
+                "4. Maquinaria \n");
+                int secc = scanner.nextInt();
+                System.out.println("Ingrese el ID del contenedor: ");
+                int id = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Ingrese el producto: ");
+                String product = scanner.nextLine();
+                scanner.nextLine();
+                System.out.println("Ingrese el peso del contenedor: ");
+                double peso = scanner.nextDouble();
+                scanner.nextLine();
+                System.out.println("Ingrese el destino del contenedor: ");
+                String destiny = scanner.nextLine();
+                scanner.nextLine();
+
+                Contenedor cont = new Contenedor(id, product, peso, destiny);
+
+                switch (secc) {
+                    case 1:
+                        System.out.println("Productos Electrónicos");
+                        sec1.agregarContenedor(cont);
+                        System.out.println("El contenedor fue agregado exitosamente");
+                        break;
+                    case 2:
+                        System.out.println("Ropa");
+                        sec2.agregarContenedor(cont);
+                        System.out.println("El contenedor fue agregado exitosamente");
+                        break;
+                    case 3:
+                        System.out.println("Alimentos");
+                        sec3.agregarContenedor(cont);
+                        System.out.println("El contenedor fue agregado exitosamente");
+                        break;
+                    case 4:
+                        System.out.println("Maquinaria");
+                        sec4.agregarContenedor(cont);
+                        System.out.println("El contenedor fue agregado exitosamente");
+                        break;
+                    default:
+                        System.out.println("Seleccione una seccion válida");
+                        break;
+                }
                 break;
             case 2:
-                System.err.println("Mostrar Información de los Contenedores");
+                System.out.println("Mostrar Información de los Contenedores");
                 break;
 
             case 3:
-                System.err.println("Mover Contenedores");
+                System.out.println("Mover Contenedores");
                 break;
 
             case 4:
-                System.err.println("Calcular el Peso Total por Sección");
+                System.out.println("Calcular el Peso Total por Sección");
                 break;
         
             default:
-                System.err.println("Elija una opción válida");
+                System.out.println("Elija una opción válida");
                 break;
         }
 
